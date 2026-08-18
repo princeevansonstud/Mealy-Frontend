1a94a0b2bd1826b68602b293d16251e3e2afab55
adisacodes <marianavugwe@gmail.com>
2026-08-18 10:23:47 +0300

    add menu slice with mock data and register in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import activeTabReducer from './slices/activeTabSlice';
+import menuReducer from './slices/menuSlice';
+
 
 export const store = configureStore({
     reducer: {
         activeTab: activeTabReducer,
+        menu: menuReducer,
     },
 });
