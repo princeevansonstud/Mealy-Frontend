510621b0fbdae040fe4a38d93f8e7b8531416ade
precious <precious.maina@student.moringaschool.com>
2026-08-18 10:17:48 +0300

    add mealManagementReducer to central Redux store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import activeTabReducer from './slices/activeTabSlice';
+import mealManagementReducer from './slices/mealManagementSlice';
 
 export const store = configureStore({
-    reducer: {
-        activeTab: activeTabReducer,
-    },
+  reducer: {
+    activeTab: activeTabReducer,
+    mealManagement: mealManagementReducer,
+  },
 });
