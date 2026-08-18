17502c72b1f873521ff2c1fa88883418f2bcccf2
precious <precious.maina@student.moringaschool.com>
2026-08-18 10:41:38 +0300

    implement AdminDashboard layout

---
+++ src/pages /AdminDashboardPage.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import TabNavigation from '../layout/TabNavigation';
+import ManageMealsTab from './ManageMealsTab';
+import SetupMenuTab from './SetupMenuTab';
+import ManageOrdersTab from './ManageOrdersTab';
+import RevenueTab from './RevenueTab';
+
+export default function AdminDashboard() {
+    const currentTab = useSelector((state) => state.activeTab.currentTab);
+
+    const renderTabContent = () => {
+        switch (currentTab) {
+            case 'manage-meals':
+                return <ManageMealsTab />;
+            case 'setup-menu':
+                return <SetupMenuTab />;
+            case 'manage-orders':
+                return <ManageOrdersTab />;
+            case 'revenue':
+                return <RevenueTab />;
+            default:
+                return <ManageMealsTab />;
+        }
+    };
+
+    return (
+        <div className="w-full">
+            <TabNavigation userRole="admin" />
+            <div className="mt-6">
+                {renderTabContent()}
+            </div>
+        </div>
+    );
+}
