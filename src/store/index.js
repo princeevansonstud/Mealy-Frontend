49613bb058de2d1b62cba4de582c42a47d9fe4e5
Safia Bulle <safia.bulle@student.moringaschool.com>
2026-08-18 11:28:20 +0300

    feat: add authentication state

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import activeTabReducer from './slices/activeTabSlice';
+import authReducer from './slices/authSlice';
 
 export const store = configureStore({
-    reducer: {
-        activeTab: activeTabReducer,
-    },
+  reducer: {
+    activeTab: activeTabReducer,
+    auth: authReducer,
+  },
 });
